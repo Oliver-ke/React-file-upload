10db5cdc740721a2f1b3a58f94de176e2e5510e7
azorji kelechi oliver <kelechioliver96@gmail.com>
2019-05-16 01:54:37 +0100

    commented server file

--- test/server-spec.js
+++ test/server-spec.js
@@ -1,5 +1,5 @@
 const expect = require('chai').expect;
-const server = require('../server');
+//const server = require('../server');
 
 describe('test', () => {
 	it('should return a string', () => {
