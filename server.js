8db5291d212944a093701f6ff88a585d769815ce
azorji kelechi oliver <kelechioliver96@gmail.com>
2019-05-24 19:38:07 +0100

    heroku  integration with contineus deploy

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ cloudinary.config({
 });
 
 app.use('/media', mediaRoute);
-
+app.get('*', (req, res) => res.json({ status: 200, mssg: 'working' }));
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
 	console.log(`server running on port ${PORT}`);
